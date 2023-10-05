bee68e8511cbee91ddf8aa60845b6cf872b6050a
Rory <yourboyrory@gmail.com>
2023-10-05 12:30:01 -0400

    started address leds with GUI feature

--- configGUI.sh
+++ configGUI.sh
@@ -8,8 +8,10 @@ ramTmp="/tmp/ram_$plug.tmp"
 mkdir ~/.config/rgbFanDriver/ 2> /dev/null
 cd ~/.config/rgbFanDriver/
 caseCfg="./case.cfg"
+caseLedCfg="./caseLED.cfg"
 aioCfg="./aio.cfg"
 ramCfg="./ram.cfg"
+ramLedCfg="./ramLED.cfg"
 
 touch $caseCfg
 touch $aioCfg
@@ -32,7 +34,6 @@ setCBValue() {
 }
 
 readConfig() {
-    
     # Case Var
     caseCLRMode=$(grep -oP '^caseCLRMode=\K.*' $caseCfg | head -n1 )
     caseCLRSpeed=$(grep -oP '^caseCLRSpeed=\K.*' $caseCfg | head -n1 )
@@ -40,6 +41,7 @@ readConfig() {
     caseSecondaryCLR=$(grep -oP '^caseSecondaryCLR=\K.*' $caseCfg | head -n1 )
     caseTertiaryCLR=$(grep -oP '^caseTertiaryCLR=\K.*' $caseCfg | head -n1 )
     caseQuaternaryCLR=$(grep -oP '^caseQuaternaryCLR=\K.*' $caseCfg | head -n1 )
+    caseSpeedNotify=$(grep -oP '^caseSpeedNotify=\K.*' $caseCfg | head -n1 )
     caseTempProbe=$(grep -oP '^caseTempProbe=\K.*' $caseCfg | head -n1 )
     caseSpeed20=$(grep -oP '^caseSpeed20=\K.*' $caseCfg | head -n1 )
     caseSpeed30=$(grep -oP '^caseSpeed30=\K.*' $caseCfg | head -n1 )
@@ -56,12 +58,12 @@ readConfig() {
     aioSecondaryCLR=$(grep -oP '^aioSecondaryCLR=\K.*' $aioCfg | head -n1 )
     aioPumpSpeed=$(grep -oP '^aioPumpSpeed=\K.*' $aioCfg | head -n1 )
     aioTempProbe=$(grep -oP '^aioTempProbe=\K.*' $aioCfg | head -n1 )
-    aioSpeed25=$(grep -oP '^aioSpeed25=\K.*' $aioCfg | head -n1 )
-    aioSpeed35=$(grep -oP '^aioSpeed35=\K.*' $aioCfg | head -n1 )
-    aioSpeed50=$(grep -oP '^aioSpeed50=\K.*' $aioCfg | head -n1 )
-    aioSpeed65=$(grep -oP '^aioSpeed65=\K.*' $aioCfg | head -n1 )
-    aioSpeed75=$(grep -oP '^aioSpeed75=\K.*' $aioCfg | head -n1 )
-    aioSpeed90=$(grep -oP '^aioSpeed90=\K.*' $aioCfg | head -n1 )
+    aioSpeed24=$(grep -oP '^aioSpeed24=\K.*' $aioCfg | head -n1 )
+    aioSpeed36=$(grep -oP '^aioSpeed36=\K.*' $aioCfg | head -n1 )
+    aioSpeed48=$(grep -oP '^aioSpeed48=\K.*' $aioCfg | head -n1 )
+    aioSpeed60=$(grep -oP '^aioSpeed60=\K.*' $aioCfg | head -n1 )
+    aioSpeed72=$(grep -oP '^aioSpeed72=\K.*' $aioCfg | head -n1 )
+    aioSpeed84=$(grep -oP '^aioSpeed84=\K.*' $aioCfg | head -n1 )
 
     # Ram Var
     disableRamOnSleep=$(grep -oP '^disableRamOnSleep=\K.*' $ramCfg | head -n1 )
@@ -69,7 +71,6 @@ readConfig() {
     ramPrimaryCLR=$(grep -oP '^ramPrimaryCLR=\K.*' $ramCfg | head -n1 )
     ramSecondaryCLR=$(grep -oP '^ramSecondaryCLR=\K.*' $ramCfg | head -n1 )
     ramLEDBrightness=$(grep -oP '^ramLEDBrightness=\K.*' $ramCfg | head -n1 )
-
 }
 
 writeConfig() {
@@ -81,15 +82,16 @@ writeConfig() {
     cat $caseTmp | awk '{ print "caseSecondaryCLR="$4 }' >> $caseCfg
     cat $caseTmp | awk '{ print "caseTertiaryCLR="$5 }' >> $caseCfg
     cat $caseTmp | awk '{ print "caseQuaternaryCLR="$6 }' >> $caseCfg
-    cat $caseTmp | awk '{ print "caseTempProbe="$7 }' >> $caseCfg
-    cat $caseTmp | awk '{ print "caseSpeed20="$8 }' >> $caseCfg
-    cat $caseTmp | awk '{ print "caseSpeed30="$9 }' >> $caseCfg
-    cat $caseTmp | awk '{ print "caseSpeed40="$10 }' >> $caseCfg
-    cat $caseTmp | awk '{ print "caseSpeed50="$11 }' >> $caseCfg
-    cat $caseTmp | awk '{ print "caseSpeed60="$12 }' >> $caseCfg
-    cat $caseTmp | awk '{ print "caseSpeed70="$13 }' >> $caseCfg
-    cat $caseTmp | awk '{ print "caseSpeed80="$14 }' >> $caseCfg
-    cat $caseTmp | awk '{ print "caseSpeed90="$15 }'  >> $caseCfg
+    cat $caseTmp | awk '{ print "caseSpeedNotify="$7 }' >> $caseCfg
+    cat $caseTmp | awk '{ print "caseTempProbe="$8 }' >> $caseCfg
+    cat $caseTmp | awk '{ print "caseSpeed20="$9 }' >> $caseCfg
+    cat $caseTmp | awk '{ print "caseSpeed30="$10 }' >> $caseCfg
+    cat $caseTmp | awk '{ print "caseSpeed40="$11 }' >> $caseCfg
+    cat $caseTmp | awk '{ print "caseSpeed50="$12 }' >> $caseCfg
+    cat $caseTmp | awk '{ print "caseSpeed60="$13 }' >> $caseCfg
+    cat $caseTmp | awk '{ print "caseSpeed70="$14 }' >> $caseCfg
+    cat $caseTmp | awk '{ print "caseSpeed80="$15 }' >> $caseCfg
+    cat $caseTmp | awk '{ print "caseSpeed90="$16 }'  >> $caseCfg
     
     # AIO Key/Value
     cat $aioTmp | awk '{ print "aioCLRMode="$1 }' > $aioCfg
@@ -97,12 +99,12 @@ writeConfig() {
     cat $aioTmp | awk '{ print "aioSecondaryCLR="$3 }' >> $aioCfg
     cat $aioTmp | awk '{ print "aioPumpSpeed="$4 }' >> $aioCfg
     cat $aioTmp | awk '{ print "aioTempProbe="$5 }' >> $aioCfg
-    cat $aioTmp | awk '{ print "aioSpeed25="$6 }' >> $aioCfg
-    cat $aioTmp | awk '{ print "aioSpeed35="$7 }' >> $aioCfg
-    cat $aioTmp | awk '{ print "aioSpeed50="$8 }' >> $aioCfg
-    cat $aioTmp | awk '{ print "aioSpeed65="$9 }' >> $aioCfg
-    cat $aioTmp | awk '{ print "aioSpeed75="$10 }' >> $aioCfg
-    cat $aioTmp | awk '{ print "aioSpeed90="$11 }' >> $aioCfg
+    cat $aioTmp | awk '{ print "aioSpeed24="$6 }' >> $aioCfg
+    cat $aioTmp | awk '{ print "aioSpeed36="$7 }' >> $aioCfg
+    cat $aioTmp | awk '{ print "aioSpeed48="$8 }' >> $aioCfg
+    cat $aioTmp | awk '{ print "aioSpeed60="$9 }' >> $aioCfg
+    cat $aioTmp | awk '{ print "aioSpeed72="$10 }' >> $aioCfg
+    cat $aioTmp | awk '{ print "aioSpeed84="$11 }' >> $aioCfg
     
     # Ram Key/Value
     cat $ramTmp | awk '{ print "disableRamOnSleep="$1 }' > $ramCfg
@@ -125,7 +127,7 @@ aioCLRMode=$(setCBValue "fixed!fading!blinking!blackout" "$aioCLRMode")
 aioTempProbe=$(setCBValue "cpu!coolant" "$aioTempProbe")
 
 # Fix Ram Var
-ramCLRMode=$(setCBValue "direct!rainbow!visor!rain!marquee!sequential!rainbow-wave!color-shift!color-pulse!" "$ramCLRMode")
+ramCLRMode=$(setCBValue "direct!static!rainbow!visor!rain!marquee!sequential!rainbow-wave!color-shift!color-pulse!" "$ramCLRMode")
     
     # Case
     (
@@ -137,8 +139,22 @@ ramCLRMode=$(setCBValue "direct!rainbow!visor!rain!marquee!sequential!rainbow-wa
         --field="LED Secondary Color":CLR "$caseSecondaryCLR"\
         --field="LED Tertiary Color":CLR "$caseTertiaryCLR"\
         --field="LED Quaternary Color":CLR "$caseQuaternaryCLR"\
-        --field="Address LEDs":BTN "" \
+        --field="Address LEDs"!"color-management"!"":BTN "bash -c 'cat caseAddrRGB.cfg | \
+            yad --form --window-icon=color-management --title=\"Adress LED\" --columns=4 --button=Clear:1 --button=Save:0 --separator=\"\n\" \
+            --field=Enable:SW \
+            --field=LED_1:CLR --field=LED_2:CLR --field=LED_3:CLR --field=LED_4:CLR \
+            --field=LED_5:CLR --field=LED_6:CLR --field=LED_7:CLR --field=LED_8:CLR \
+            --field=LED_9:CLR --field=LED_10:CLR --field=\" \":LBL --field=\" \":LBL --field=LED_11:CLR --field=LED_12:CLR \
+            --field=LED_13:CLR --field=LED_14:CLR --field=LED_15:CLR --field=LED_16:CLR \
+            --field=LED_17:CLR --field=LED_18:CLR --field=LED_19:CLR --field=LED_20:CLR  --field=\" \":LBL --field=\" \":LBL \
+            --field=LED_21:CLR --field=LED_22:CLR --field=LED_23:CLR --field=LED_24:CLR \
+            --field=LED_25:CLR --field=LED_26:CLR --field=LED_27:CLR --field=LED_28:CLR \
+            --field=LED_29:CLR --field=LED_30:CLR --field=\" \":LBL --field=\" \":LBL --field=LED_31:CLR --field=LED_32:CLR \
+            --field=LED_33:CLR --field=LED_34:CLR --field=LED_35:CLR --field=LED_36:CLR \
+            --field=LED_37:CLR --field=LED_38:CLR --field=LED_39:CLR --field=LED_40:CLR \
+            | tee caseAddrRGB.cfg'" \
         --field="<b>Fan Curve</b>":LBL ""\
+        --field="Notify on change":SW "$caseSpeedNotify"\
         --field="Tempature Probe":CB $caseTempProbe \
         --field="Speed at 20c":SCL "$caseSpeed20"\
         --field="Speed at 30c":SCL "$caseSpeed30"\
@@ -154,19 +170,19 @@ ramCLRMode=$(setCBValue "direct!rainbow!visor!rain!marquee!sequential!rainbow-wa
     (
         yad --form --separator=" " --plug=$plug --tabnum=2\
         --field="<b>LOGO LED Options</b>":LBL ""\
-        --field="LED Color Mode":CB $aioCLRMode \
+      q  --field="LED Color Mode":CB $aioCLRMode \
         --field="LED Primary Color":CLR "$aioPrimaryCLR"\
         --field="LED Secondary Color":CLR "$aioSecondaryCLR"\
         --field="<b>Pump Control</b>":LBL ""\
         --field="Pump Speed":SCL "$aioPumpSpeed"\
         --field="<b>Fan Curve</b>":LBL ""\
         --field="Tempature Probe":CB $aioTempProbe \
-        --field="Speed at 25c":SCL "$aioSpeed25"\
-        --field="Speed at 35c":SCL "$aioSpeed35"\
-        --field="Speed at 50c":SCL "$aioSpeed50"\
-        --field="Speed at 65c":SCL "$aioSpeed65"\
-        --field="Speed at 75c":SCL "$aioSpeed75"\
-        --field="Speed at 90c":SCL "$aioSpeed90"\
+        --field="Speed at 24c":SCL "$aioSpeed24"\
+        --field="Speed at 36c":SCL "$aioSpeed36"\
+        --field="Speed at 48c":SCL "$aioSpeed48"\
+        --field="Speed at 60c":SCL "$aioSpeed60"\
+        --field="Speed at 72c":SCL "$aioSpeed72"\
+        --field="Speed at 84c":SCL "$aioSpeed84"\
         >$aioTmp
     )&\
     # RAM
@@ -178,22 +194,45 @@ ramCLRMode=$(setCBValue "direct!rainbow!visor!rain!marquee!sequential!rainbow-wa
         --field="LED Brightness":SCL "$ramLEDBrightness"\
         --field="LED Primary Color":CLR "$ramPrimaryCLR"\
         --field="LED Secondary Color":CLR "$ramSecondaryCLR"\
-        --field="Address LEDs":BTN "" \
+        --field="Address LEDs"!"color-management"!"":BTN "bash -c 'cat rgbAddrRGB.cfg | \
+            yad --form --window-icon=color-management --title=\"Adress LED\" --button=Clear:1 --button=Save:0 --separator=\"\n\" \
+            --field=Enable:SW \
+            --field=LED_1:CLR \
+            --field=LED_2:CLR \
+            --field=LED_3:CLR \
+            --field=LED_4:CLR \
+            --field=LED_5:CLR \
+            --field=LED_6:CLR \
+            --field=LED_7:CLR \
+            --field=LED_8:CLR \
+            --field=LED_9:CLR \
+            | tee rgbAddrRGB.cfg'" \
         >$ramTmp
     )&\
     
     #draw UI
     (
-        yad --notebook --key=$plug --width=500 --title="$userName: $(hostname) Config"\
+        yad --notebook --window-icon=configure --key=$plug --width=600 --title="$userName: $(hostname) Config"\
         --tab="Case"\
         --tab="AIO"\
         --tab="RAM"\
-        --button="Save":0\
-        --button="Cancel":1\
+        --button="Apply"!!"Restarts the services to apply the changes now.":2\
+        --button="Save"!!"Saves the config and exits but doesn't restart the services.":0\
+        --button="Cancel"!!"Doesn't Save anything and exit.":1\
     )
+    exitCode=$?
     
-if [[ $? -eq 0 ]] ; then
+if [[ $exitCode -eq 0 ]] ; then
     writeConfig
+elif [[ $exitCode -eq 2 ]] ; then
+    killall -9 rgbfandriver
+    writeConfig
+    (
+        rgbfandriver -q
+    )&
+    (
+        rgbfandriver-config
+    )&
 fi
 
 rm $caseTmp
--- rgbfandriver.sh
+++ rgbfandriver.sh
@@ -7,7 +7,6 @@ aioCfg="./aio.cfg"
 ramCfg="./ram.cfg"
 
 readConfig() {
-    
     # Case Var
     caseCLRMode=$(grep -oP '^caseCLRMode=\K.*' $caseCfg | head -n1 )
     caseCLRSpeed=$(grep -oP '^caseCLRSpeed=\K.*' $caseCfg | head -n1 )
@@ -15,6 +14,7 @@ readConfig() {
     caseSecondaryCLR=$(grep -oP '^caseSecondaryCLR=\K.*' $caseCfg | head -n1 )
     caseTertiaryCLR=$(grep -oP '^caseTertiaryCLR=\K.*' $caseCfg | head -n1 )
     caseQuaternaryCLR=$(grep -oP '^caseQuaternaryCLR=\K.*' $caseCfg | head -n1 )
+    caseSpeedNotify=$(grep -oP '^caseSpeedNotify=\K.*' $caseCfg | head -n1 )
     caseTempProbe=$(grep -oP '^caseTempProbe=\K.*' $caseCfg | head -n1 )
     caseSpeed20=$(grep -oP '^caseSpeed20=\K.*' $caseCfg | head -n1 )
     caseSpeed30=$(grep -oP '^caseSpeed30=\K.*' $caseCfg | head -n1 )
@@ -31,12 +31,12 @@ readConfig() {
     aioSecondaryCLR=$(grep -oP '^aioSecondaryCLR=\K.*' $aioCfg | head -n1 )
     aioPumpSpeed=$(grep -oP '^aioPumpSpeed=\K.*' $aioCfg | head -n1 )
     aioTempProbe=$(grep -oP '^aioTempProbe=\K.*' $aioCfg | head -n1 )
-    aioSpeed25=$(grep -oP '^aioSpeed25=\K.*' $aioCfg | head -n1 )
-    aioSpeed35=$(grep -oP '^aioSpeed35=\K.*' $aioCfg | head -n1 )
-    aioSpeed50=$(grep -oP '^aioSpeed50=\K.*' $aioCfg | head -n1 )
-    aioSpeed65=$(grep -oP '^aioSpeed65=\K.*' $aioCfg | head -n1 )
-    aioSpeed75=$(grep -oP '^aioSpeed75=\K.*' $aioCfg | head -n1 )
-    aioSpeed90=$(grep -oP '^aioSpeed90=\K.*' $aioCfg | head -n1 )
+    aioSpeed24=$(grep -oP '^aioSpeed24=\K.*' $aioCfg | head -n1 )
+    aioSpeed36=$(grep -oP '^aioSpeed36=\K.*' $aioCfg | head -n1 )
+    aioSpeed48=$(grep -oP '^aioSpeed48=\K.*' $aioCfg | head -n1 )
+    aioSpeed60=$(grep -oP '^aioSpeed60=\K.*' $aioCfg | head -n1 )
+    aioSpeed72=$(grep -oP '^aioSpeed72=\K.*' $aioCfg | head -n1 )
+    aioSpeed84=$(grep -oP '^aioSpeed84=\K.*' $aioCfg | head -n1 )
 
     # Ram Var
     disableRamOnSleep=$(grep -oP '^disableRamOnSleep=\K.*' $ramCfg | head -n1 )
@@ -44,40 +44,37 @@ readConfig() {
     ramPrimaryCLR=$(grep -oP '^ramPrimaryCLR=\K.*' $ramCfg | head -n1 )
     ramSecondaryCLR=$(grep -oP '^ramSecondaryCLR=\K.*' $ramCfg | head -n1 )
     ramLEDBrightness=$(grep -oP '^ramLEDBrightness=\K.*' $ramCfg | head -n1 )
-
 }
 
 readConfig
 
-if [[ $1 == "-n" ]]; then
-    notify="true"
-else
-    notify="false"
-fi
-
 if [[ $1 == "-h" ]]; then
-    echo "-n    makes a notification happen when fan changes power state"
+    echo "  "
+    echo "-q    Starts the program without Initializing hardware, use this when restarting the script."
+    echo "  "
 fi
 
-echo "[INFO] Initializing hardware..."
-liquidctl --match "NZXT Smart Device" initialize
-sleep 5
+if [[ $1 != "-q" ]]; then
+    echo "[INFO] Initializing hardware..."
+    liquidctl --match "NZXT Smart Device" initialize
+    sleep 5
+fi
 
 # Water Cooler
 liquidctl --match "Corsair Hydro H100i" set led color $aioCLRMode "$aioPrimaryCLR" "$caseSecondaryCLR"
 liquidctl --match "Corsair Hydro H100i" set pump speed $aioPumpSpeed
-if liquidctl --match "Corsair Hydro H100i" set fan speed 25 $aioSpeed25 35 $aioSpeed35 50 $aioSpeed50 65 $aioSpeed65 75 $aioSpeed75 90 $aioSpeed90 ; then
+if liquidctl --match "Corsair Hydro H100i" set fan speed 24 $aioSpeed24 36 $aioSpeed36 48 $aioSpeed48 60 $aioSpeed60 72 $aioSpeed72 84 $aioSpeed84 ; then
     echo " "
     echo "=============================="  
     echo "Temp Probe Set To: $aioTempProbe" 
     echo "=============================="
     echo "Set AIO fan speed table"
-    echo "25c | $aioSpeed25% RPM"
-    echo "35c | $aioSpeed35% RPM"
-    echo "50c | $aioSpeed50% RPM"
-    echo "65c | $aioSpeed65% RPM"
-    echo "75c | $aioSpeed75% RPM"
-    echo "90c | $aioSpeed90% RPM"
+    echo "24c | $aioSpeed24% RPM"
+    echo "36c | $aioSpeed36% RPM"
+    echo "48c | $aioSpeed48% RPM"
+    echo "60c | $aioSpeed60% RPM"
+    echo "72c | $aioSpeed72% RPM"
+    echo "84c | $aioSpeed84% RPM"
     echo "=============================="
     echo "Pump Speed | $aioPumpSpeed% RPM"
     echo "=============================="
@@ -89,7 +86,6 @@ fi
 
 # Case 
 liquidctl --match "NZXT Smart Device" set led color $caseCLRMode "$casePrimaryCLR" "$caseSecondaryCLR" "$caseTertiaryCLR" "$caseQuaternaryCLR" --speed $caseCLRSpeed
-lastPowerState=0
 
     echo " "
     echo "=============================="  
@@ -110,89 +106,95 @@ lastPowerState=0
 # RAM
 echo "[INFO] Spinning off RAM RGB workers..."
 for i in {0..3}; do
-	openrgb -d $i -m $ramCLRMode -c ${ramPrimaryCLR:1} -c ${ramSecondaryCLR:1} -b $ramLEDBrightness --noautoconnect &
+	openrgb -d $i -m $ramCLRMode -c ${ramPrimaryCLR:1} -b $ramLEDBrightness --noautoconnect &
 done
 
 # Fan
-if [ $notify = "true" ] ; then
-    notifyID=$(notify-send -p --urgency=low  --app-name= "Fan controller Low Power" "Fan Speed 20")
-fi
-
+notifyID=0
+lastPowerState=0
+icon20="power-profile-power-saver-symbolic"
+icon30="power-profile-power-saver-symbolic"
+icon40="power-profile-power-saver-symbolic"
+icon50="power-profile-balanced-symbolic"
+icon60="power-profile-balanced-symbolic"
+icon70="power-profile-performance-symbolic"
+icon80="power-profile-performance-symbolic"
+icon90="power-profile-performance-symbolic"
 while true; do
-    if [ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 30 ]; then
-        if [ $lastPowerState -ne 20 ]; then
+    if [[ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 30 ]]; then
+        if [[ $lastPowerState -ne 20 ]]; then
             liquidctl --match "NZXT Smart Device" set sync speed $caseSpeed20
             echo "[INFO] GPU Temp 20c: Fan Speed $caseSpeed20"
             lastPowerState=20
-            if [ $notify = "true" ]; then
-                notifyID=$(notify-send -p --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 20c" "Fan Speed $caseSpeed20")
+            if [[ $caseSpeedNotify == "TRUE" ]]; then
+                notifyID=$(notify-send -p --icon="$icon20" --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 20c" "Fan Speed $caseSpeed20")
             fi
         fi
-    elif [ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 40 ]; then
-        if [ $lastPowerState -ne 30 ]; then
+    elif [[ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 40 ]]; then
+        if [[ $lastPowerState -ne 30 ]]; then
             liquidctl --match "NZXT Smart Device" set sync speed $caseSpeed30
             echo "[INFO] GPU Temp 30c: Fan Speed $caseSpeed30"
             lastPowerState=30
-            if [ $notify = "true" ]; then
-                notifyID=$(notify-send -p --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 30c" "Fan Speed $caseSpeed30")
+            if [[ $caseSpeedNotify == "TRUE" ]]; then
+                notifyID=$(notify-send -p --icon="$icon30" --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 30c" "Fan Speed $caseSpeed30")
             fi
         fi
-    elif [ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 50 ]; then
-        if [ $lastPowerState -ne 40 ]; then
+    elif [[ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 50 ]]; then
+        if [[ $lastPowerState -ne 40 ]]; then
             liquidctl --match "NZXT Smart Device" set sync speed $caseSpeed40
             echo "[INFO] GPU Temp 40c: Fan Speed $caseSpeed40"
             lastPowerState=40
-            if [ $notify = "true" ]; then
-                notifyID=$(notify-send -p --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 40c" "Fan Speed $caseSpeed40")
+            if [[ $caseSpeedNotify == "TRUE" ]]; then
+                notifyID=$(notify-send -p --icon="$icon40" --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 40c" "Fan Speed $caseSpeed40")
             fi
         fi
-    elif [ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 60 ]; then
-        if [ $lastPowerState -ne 50 ]; then
+    elif [[ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 60 ]]; then
+        if [[ $lastPowerState -ne 50 ]]; then
             liquidctl --match "NZXT Smart Device" set sync speed $caseSpeed50
             echo "[INFO] GPU Temp 50c: Fan Speed $caseSpeed50"
             lastPowerState=50
-            if [ $notify = "true" ]; then
-                notifyID=$(notify-send -p --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 50c" "Fan Speed $caseSpeed50")
+            if [[ $caseSpeedNotify == "TRUE" ]]; then
+                notifyID=$(notify-send -p --icon="$icon50" --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 50c" "Fan Speed $caseSpeed50")
             fi
         fi
-    elif [ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 70 ]; then
-        if [ $lastPowerState -ne 60 ]; then
+    elif [[ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 70 ]]; then
+        if [[ $lastPowerState -ne 60 ]]; then
             liquidctl --match "NZXT Smart Device" set sync speed $caseSpeed60
             echo "[INFO] GPU Temp 60c: Fan Speed $caseSpeed60"
             lastPowerState=60
-            if [ $notify = "true" ]; then
-                notifyID=$(notify-send -p --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 60c" "Fan Speed $caseSpeed60")
+            if [[ $caseSpeedNotify == "TRUE" ]]; then
+                notifyID=$(notify-send -p --icon="$icon60" --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 60c" "Fan Speed $caseSpeed60")
             fi
         fi
-    elif [ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 80 ]; then
-        if [ $lastPowerState -ne 70 ]; then
+    elif [[ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 80 ]]; then
+        if [[ $lastPowerState -ne 70 ]]; then
             liquidctl --match "NZXT Smart Device" set sync speed $caseSpeed70
             echo "[INFO] GPU Temp 70c: Fan Speed $caseSpeed70"
             lastPowerState=70
-            if [ $notify = "true" ]; then
-                notifyID=$(notify-send -p --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 70c" "Fan Speed $caseSpeed70")
+            if [[ $caseSpeedNotify == "TRUE" ]]; then
+                notifyID=$(notify-send -p --icon="$icon70" --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 70c" "Fan Speed $caseSpeed70")
             fi
         fi
-    elif [ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 90 ]; then
-        if [ $lastPowerState -ne 80 ]; then
+    elif [[ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -lt 90 ]]; then
+        if [[ $lastPowerState -ne 80 ]]; then
             liquidctl --match "NZXT Smart Device" set sync speed $caseSpeed80
             echo "[INFO] GPU Temp 80c: Fan Speed $caseSpeed80"
             lastPowerState=80
-            if [ $notify = "true" ]; then
-                notifyID=$(notify-send -p --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 80c" "Fan Speed $caseSpeed80")
+            if [[ $caseSpeedNotify == "TRUE" ]]; then
+                notifyID=$(notify-send -p --icon="$icon80" --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 80c" "Fan Speed $caseSpeed80")
             fi
         fi
-    elif [ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -gt 89 ]; then
-        if [ $lastPowerState -ne 90 ]; then
+    elif [[ $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader) -gt 89 ]]; then
+        if [[ $lastPowerState -ne 90 ]]; then
             liquidctl --match "NZXT Smart Device" set sync speed $caseSpeed90
             echo "[INFO] GPU High Power state: Fan Speed 100"
             lastPowerState=90
-            if [ $notify = "true" ]; then
-                notifyID=$(notify-send -p --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 90c" "Fan Speed $caseSpeed90")
+            if [[ $caseSpeedNotify == "TRUE" ]]; then
+                notifyID=$(notify-send -p --icon="$icon90" --replace-id=$notifyID --urgency=low  --app-name= "GPU Temp 90c" "Fan Speed $caseSpeed90")
             fi
         fi
     else
-        if [ $lastPowerState -ne 90 ]; then
+        if [[ $lastPowerState -ne 90 ]]; then
             liquidctl --match "NZXT Smart Device" set sync speed $caseSpeed90
             echo "[WARN] Cannot communicate with nvidia driver, defaulting to high power state"
             notify-send -p --urgency=critical  --app-name= "Fan controller defaulting to highest fan speed" "Script cannot communicate with nvidia driver"
