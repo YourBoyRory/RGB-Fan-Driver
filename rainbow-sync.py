1adcfccb8764fce90a08edbd029334eca4554d28
YourBoyRory <yourboyrory@gmail.com>
2024-05-29 00:41:50 -0400

    cleaned up code

--- rainbow-sync.py
+++ rainbow-sync.py
@@ -6,23 +6,14 @@ from threading import Thread
 from openrgb import OpenRGBClient
 from openrgb.utils import RGBColor, DeviceType
 
-clients = OpenRGBClient()
-RAM = clients.get_devices_by_type(DeviceType.DRAM)
-AIO = clients.get_devices_by_type(DeviceType.COOLER)
+# Wave and Color Settings
+color_brightness=80 # 0-255
+color_desaturation=0 # 0-255
 
-for module in RAM:
-    module.set_mode("direct")
-
-step=8
-speed=.1
-jumpStep=4
-brightness=80
-desaturation=0
-
-red=brightness
-green=desaturation
-blue=desaturation
-
+# Preset Values, Change only if you want to change effect speed
+effect_smoothness=int(abs(color_brightness-color_desaturation)/10) # 1 is perfectly smooth, higher is less smooth, brings down CPU usage
+effect_slowness=effect_smoothness/color_brightness # calculates the delay between "frames"
+wave_frequency=(abs(color_brightness-color_desaturation)/2)/effect_smoothness # changes the waves frequency higher means more colors visible at once
 
 def limit(value):
     if value > 255:
@@ -49,54 +40,68 @@ def spawnWorkers(colors):
     ]
     for w in workers:
         w.start()
-    time.sleep(speed)
+    time.sleep(effect_slowness)
     for w in workers:
         w.join()
 
-def generateColor(lookAhead, red, green, blue):
-    count=(jumpStep*lookAhead)
+def generateColor(lookAhead,color):
+    count=(wave_frequency*lookAhead)
+    red=color.red
+    green=color.green
+    blue=color.blue
     while count > -1:
-        if green >= brightness and blue < brightness:
-            if red > desaturation:
-                red=red-step
+        if green >= color_brightness and blue < color_brightness:
+            if red > color_desaturation:
+                red=red-effect_smoothness
             else:
-                blue=blue+step
-        elif blue >= brightness and red < brightness:
-            if green > desaturation:
-                green=green-step
+                blue=blue+effect_smoothness
+        elif blue >= color_brightness and red < color_brightness:
+            if green > color_desaturation:
+                green=green-effect_smoothness
             else:
-                red=red+step
-        elif red >= brightness and green < brightness:
-            if blue > desaturation:
-                blue=blue-step
+                red=red+effect_smoothness
+        elif red >= color_brightness and green < color_brightness:
+            if blue > color_desaturation:
+                blue=blue-effect_smoothness
             else:
-                green=green+step
+                green=green+effect_smoothness
         count=count-1
-    #print("color", red,green,blue)
-    return red,green,blue,RGBColor(limit(red),limit(green),limit(blue))
+    return RGBColor(limit(red),limit(green),limit(blue))
 
-def rainbow(red, green, blue):
-    colors = [0] * 10
-    newRed,newGreen,newBlue,colors[0] = generateColor(0,red,green,blue)
-    count = 1
-    while count < 10:
-        redDmp,greenDmp,bluedmp,colors[count] = generateColor(count,red,green,blue)
+def rainbow(colors):
+    oldColor=colors[0]
+    count = 0
+    while count < len(colors):
+        colors[count] = generateColor(count,oldColor)
         count=count+1
     spawnWorkers(colors)
-    return newRed,newGreen,newBlue
+    return colors
 
 
 def dark():
     for module in RAM:
         module.clear()
 
-print(f"Brightness: \033[33m{brightness/255*100:.0f}%\033[0m")
-print(f"Saturation: \033[33m{100-(desaturation/255*100):.0f}%\033[0m")
-print(f"Step: \033[33m{step} ({((brightness-desaturation)/step)*3:.0f} total, ~{(((brightness-desaturation)/step)*3)*.6:.0f} seconds)\033[0m")
+clients = OpenRGBClient()
+RAM = clients.get_devices_by_type(DeviceType.DRAM)
+AIO = clients.get_devices_by_type(DeviceType.COOLER)
+
+# Starting Values
+currentColors = [None] * len(RAM[0].leds)
+currentColors[0] = RGBColor(limit(color_brightness),limit(color_desaturation),limit(color_desaturation))
+
+for module in RAM:
+    module.set_mode("direct")
+
+print(f"Brightness: \033[33m{color_brightness/255*100:.0f}%\033[0m")
+print(f"Saturation: \033[33m{100-(color_desaturation/255*100):.0f}%\033[0m")
+print(f"Smoothness: \033[33m{effect_smoothness} Steps ({wave_frequency:.0f}Hz, {effect_slowness*1000:.0f} milliseconds)\033[0m")
+
 while True:
     if os.path.isfile("/tmp/ram_sleep"):
         dark()
     else:
-        red,green,blue = rainbow(red,green,blue)
-        print(f"\r\033[31mR:{limit(red):3d} \033[92mG:{limit(green):3d} \033[34mB:{limit(blue):3d} \033[36m(#{limit(red):02x}{limit(green):02x}{limit(blue):02x})\033[0m ", end='', flush=True)
+        currentColors = rainbow(currentColors)
+        #print(f"\rTest\033[0m ", end='', flush=True)
+        print(f"\r\033[31mR:{currentColors[0].red:3d} \033[92mG:{currentColors[0].green:3d} \033[34mB:{currentColors[0].blue:3d} \033[36m(#{currentColors[0].red:02x}{currentColors[0].green:02x}{currentColors[0].blue:02x})\033[0m ", end='', flush=True)
 
